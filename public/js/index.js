03d1d0d80afd8d3fd0754a7e9142ac6639a19974
Manas Rawat <manasrawat.cse@gmail.com>
2022-06-17 21:12:43 +0530

    moved js to index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,21 @@
-var t = <%= isEmpty %>;
-console.log(t);
+var list = $(".list-items")[0];
+var inputField = $(".form-control")[0];
+var successToast = $(".success")[0];
+var errorToast = $(".error")[0];
+
+if(isEmpty) {
+    list.classList.remove("add-height");
+    inputField.setAttribute("placeHolder", "Add First Task")
+}
+else {
+    list.classList.add("add-height");
+    inputField.setAttribute("placeHolder", "New Task")
+}
+
+if(newItem == 1){
+    successToast.classList.remove("hidden");
+}
+
+if(newItem == -1){
+    errorToast.classList.remove("hidden");
+}
