aeffe0623770c78e5042c8937dcb79476a59a0b4
Manas Rawat <manasrawat.cse@gmail.com>
2022-06-20 12:22:15 +0530

    connected with mongodb

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/toDoList');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+const listItemsSchema = ({
+    name: String
+})
+
+const item = mongoose.model("item", listItemsSchema);
+
+var newItem = new item({
+    name: "test item"
+})
+
+// newItem.save();
+
 var newItem = 0;
-var toDoList = [];
 
 app.listen(PORT, () => {
     console.log(`working on PORT ${PORT}`);
@@ -15,13 +28,18 @@ app.listen(PORT, () => {
 
 app.get('/', (req, res) => {
     var isEmpty = true;
-    if(toDoList.length > 0) isEmpty = false;
-    res.render("index", {
-        toDoList,
-        newItem,
-        isEmpty
-        
-    });
+    item.find((err, toDoList) => {
+        if(err) console.log(err);
+        else{
+            if(toDoList.length > 0) isEmpty = false;
+            res.render("index", {
+                toDoList,
+                newItem,
+                isEmpty
+                
+            });
+        }
+    })
     newItem = 0;
 })
 
@@ -33,13 +51,21 @@ app.post('/', (req, res) => {
     }
     else{
         newItem = 1;
-        toDoList.push(entry);
+        var newItem = new item({
+            name: entry
+        })
+        newItem.save(); 
     }
     res.redirect('/');
 })
 
 app.post('/clear', (req, res) => {
-    toDoList.length = 0;
-    newItem = -2;
-    res.redirect('/');
+    item.deleteMany((err) => {
+        if(err) console.log(err);
+        else{
+            newItem = -2;
+            res.redirect('/');
+        }
+    })
+
 })
